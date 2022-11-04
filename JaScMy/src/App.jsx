a3d004ba76b14c231f557f717911758c061b86a4
Aleksey-Smouzh <xmz@tyt.by>
2022-11-04 16:39:40 +0100

    true fixed

--- JaScMy/src/App.jsx
+++ JaScMy/src/App.jsx
@@ -1,33 +1,48 @@
-import styles from "./style";
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import styles from './style'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Button from "./components/Button";
+import Business from "./components/Business";
+import CardDeal from "./components/CardDeal";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import FeedbackCard from "./components/FeedbackCard";
+import GetStarted from "./components/GetStarted";
+import Hero from "./components/Hero";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+import Billing from "./components/Billing";
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
+ 
+    <div className='bg-primary w-full overflow-hidden'>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-    
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
-);
+<div className={`${styles.paddingX} ${styles.flexCenter} `}>
+<div className={`${styles.boxWidth}`}>
+    <Navbar/>
+</div>
+</div>
 
-export default App;
+<div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+        <Hero/>
+    </div>
+</div>
+<div className={`bg-primary  ${styles.paddingX}   ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+        <Stats/>
+        <Business/>
+        <Billing/>
+        <CardDeal/>
+        <Clients/>
+        <CTA/>
+        <Footer/>
+    </div>
+</div>
+
+
+    </div>
+  
+)
+
+export default App
