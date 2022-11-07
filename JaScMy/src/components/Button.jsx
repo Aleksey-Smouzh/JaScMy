3a618dd202e027d103039f055e65298350356239
Aleksey-Smouzh <xmz@tyt.by>
2022-11-07 19:33:57 +0100

    done 4 componenets

--- JaScMy/src/components/Button.jsx
+++ JaScMy/src/components/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Buttin = () => {
+const Button = ({styles}) => {
   return (
-    <div>Buttin</div>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      Get Started</button>
   )
 }
 
-export default Buttin
+export default Button
--- JaScMy/src/components/GetStarted.jsx
+++ JaScMy/src/components/GetStarted.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import styles from '../style'
-import { arrowUp } from '../assets'
+import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
-
-        <div className={`${styles.flexStart} flex-row`}>
-          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
-            <span className='text-gradient'
-            >Get</span>
-            
-          </p>
-<img src={arrowUp} alt="arrow Up" className='w-[23px] h-[23px] object-contain' />
-
-        </div>
-        <p className="font-poppins font-medium text-[18px] leading-[23px]">
-            <span className='text-gradient'
-            >Started</span>
-            
-          </p>
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+  >
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+    >
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img
+          src={arrowUp}
+          alt="arrow Up"
+          className="w-[23px] h-[23px] object-contain"
+        />
       </div>
-
-
-
-
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
     </div>
-
-)
-export default GetStarted
+  </div>
+);
+export default GetStarted;
