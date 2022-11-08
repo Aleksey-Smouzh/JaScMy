8dd1928ad7a0c40cde920792852c35263f80887d
Aleksey-Smouzh <xmz@tyt.by>
2022-11-08 18:13:37 +0100

    done CTA

--- JaScMy/src/components/Clients.jsx
+++ JaScMy/src/components/Clients.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { clients } from '../constants'
-import styles from '../style'
+import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 const Clients = () => (
-   <section className={`${styles.flexCenter} my-4`}>
-<div className={`${styles.flexCenter} flex-wrap w-full`}>
-{clients.map((clients) => (<div key={clients.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
-  <img src={clients.logo} alt="client" className='sm:w-[192px] w-[100%] object-container' />
-</div> 
-))}
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((clients) => (
+        <div
+          key={clients.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={clients.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100%] object-container"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-</div>
-
-   </section>
-   
-  )
-
-
-export default Clients
-
+export default Clients;
