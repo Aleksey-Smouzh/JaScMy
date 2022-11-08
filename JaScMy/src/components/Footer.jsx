a5ff40f81c5fd13b2ded8de7b619e15804fb07d5
Aleksey-Smouzh <xmz@tyt.by>
2022-11-08 21:23:39 +0100

    fixed footer

--- JaScMy/src/components/Footer.jsx
+++ JaScMy/src/components/Footer.jsx
@@ -17,20 +17,20 @@ const Footer = () => (
         </p>
       </div>
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-        {footerLinks.map((footerLinks) => (
+        {footerLinks.map((footerLink) => (
           <div
-            key={footerLinks.key}
+            key={footerLink.key}
             className="flex flex-col ss:my-0 my-4 min-w-[150px]"
           >
             <h4 className=" font-poppins font-medium text-[18px] leading-[27px] text-white">
-              {footerLinks.title}
+              {footerLink.title}
             </h4>
-            <ul className="list-none mt-4">
-              {footerLinks.links.map((link, index) => (
+            <ul className="list-none mt-4" >
+              {footerLink.links.map((link, index) => (
                 <li
                   key={link.name}
                   className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerLinks.links.length - 1 ? "mb-4" : "mb-0"
+                    index !== footerLink.links.length - 1 ? "mb-4" : "mb-0"
                   }`}
                 >
                   {link.name}
